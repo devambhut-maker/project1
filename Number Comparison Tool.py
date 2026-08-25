a28dd434415ce5b17de4d0ec99cf66d090920523
devambhut-maker <devambhut@gmail.com>
2026-08-25 10:41:40 +0530

    Add files via upload

---
+++ Number Comparison Tool.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+if a > b:\
+    print("A is bigger")
+elif a== b:
+    print("Both are same")
+else:
+    print("B is bigger")
